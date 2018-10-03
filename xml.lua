202352c93f812c4b5180974641230627a5028746
DarkWiiPlayer <darkwiiplayer@hotmail.com>
2018-10-03 12:42:33 +0200

    Almost complete rewrite ;P

--- xml.lua
+++ xml.lua
@@ -1,4 +1,112 @@
 local is51 = _VERSION == 'Lua 5.1'
+local global = _ENV or _G
+local make_environment
+make_environment = function(node_handler)
+  local environment
+  do
+    environment = setmetatable({ }, {
+      __index = function(self, key)
+        local _exp_0 = key
+        if 'escape' == _exp_0 then
+          return function(...)
+            return ...
+          end
+        else
+          return global[key] or function(...)
+            return self.node(key, ...)
+          end
+        end
+      end
+    })
+  end
+  if is51 then
+    setfenv(1, environment)
+  end
+  local _ENV = _ENV and environment
+  local flatten
+  flatten = function(tab, flat)
+    if flat == nil then
+      flat = { }
+    end
+    for key, value in pairs(tab) do
+      if type(key) == "number" then
+        if type(value) == "table" then
+          flatten(value, flat)
+        else
+          flat[#flat + 1] = value
+        end
+      else
+        if type(value) == "table" then
+          flat[key] = table.concat(value(' '))
+        else
+          flat[key] = value
+        end
+      end
+    end
+    return flat
+  end
+  local inner
+  inner = function(content)
+    print("Environment: " .. tostring(_ENV))
+    for _index_0 = 1, #content do
+      local entry = content[_index_0]
+      local _exp_0 = type(entry)
+      if 'string' == _exp_0 then
+        print(escape(entry))
+      elseif 'function' == _exp_0 then
+        entry()
+      else
+        print(escape(tostring(entry)))
+      end
+    end
+  end
+  node = function(tagname, ...)
+    local arguments = flatten({
+      ...
+    })
+    local content = { }
+    for k, v in ipairs(arguments) do
+      content[k] = v
+      arguments[k] = nil
+    end
+    local handle_content
+    if #content > 0 then
+      handle_content = function()
+        return inner(content)
+      end
+    else
+      handle_content = nil
+    end
+    return node_handler(print, tagname, arguments, handle_content)
+  end
+  return environment
+end
+local call
+call = function(self, fnc)
+  if type(fnc) ~= 'function' then
+    error("Argument must be a function!", 3)
+  end
+  return error("This land is peaceful, it's inhabitants kind. But thou dost not belong.", 3)
+end
+local derive
+derive = function(self)
+  local derivate = language(self.node_handler)
+  local meta = getmetatable(derivate)
+  meta.__index = function(key)
+    return self[key] or meta.__index
+  end
+  return derivate
+end
+local language
+language = function(node_handler)
+  return setmetatable({
+    node_handler = node_handler,
+    derive = derive,
+    environment = make_environment(node_handler)
+  }, {
+    __call = call
+  })
+end
 local void
 do
   local _tbl_0 = { }
@@ -33,174 +141,46 @@ local escapes = {
   ['"'] = '&quot;',
   ["'"] = '&#039;'
 }
-local global = _ENV or _G
-local env
-env = function(make_tag)
-  local environment
+local xml = language(function(print, tag, args, inner)
+  args = table.concat((function()
+    local _accum_0 = { }
+    local _len_0 = 1
+    for key, value in pairs(args) do
+      _accum_0[_len_0] = '#{key}="#{value}"'
+      _len_0 = _len_0 + 1
+    end
+    return _accum_0
+  end)(), ' ')
+  if inner then
+    print("<" .. tostring(tag) .. " " .. tostring(args) .. ">")
+    inner()
+    return print("</" .. tostring(tag) .. ">")
+  else
+    return print("<" .. tostring(tag) .. " " .. tostring(args) .. "/>")
+  end
+end)
+local html = language(function(print, tag, args, inner)
   do
-    environment = setmetatable({ }, {
-      __index = function(self, key)
-        return global[key] or function(...)
-          return self.tag(key, ...)
-        end
-      end
-    })
-  end
-  if is51 then
-    setfenv(1, environment)
-  end
-  local _ENV = _ENV and environment
-  escape = function(value)
-    return (function(self)
-      return self
-    end)(tostring(value):gsub([[[<>&]'"]], escapes))
-  end
-  local split
-  split = function(tab)
-    local ary = { }
-    for k, v in ipairs(tab) do
-      ary[k] = v
-      tab[k] = nil
+    local _accum_0 = { }
+    local _len_0 = 1
+    for key, value in pairs(args) do
+      _accum_0[_len_0] = '#{key}="#{value}"'
+      _len_0 = _len_0 + 1
     end
-    return ary, tab
+    args = _accum_0
   end
-  local flatten
-  flatten = function(tab, flat)
-    if flat == nil then
-      flat = { }
-    end
-    for key, value in pairs(tab) do
-      if type(key) == "number" then
-        if type(value) == "table" then
-          flatten(value, flat)
-        else
-          flat[#flat + 1] = value
-        end
-      else
-        if type(value) == "table" then
-          flat[key] = table.concat(value(' '))
-        else
-          flat[key] = value
-        end
-      end
-    end
-    return flat
+  if inner then
+    print("<" .. tostring(tag) .. " " .. tostring(args) .. ">")
+    inner()
+    return print("</" .. tostring(tag) .. ">")
+  else
+    return print("<" .. tostring(tag) .. " " .. tostring(args) .. " />")
   end
-  tag = function(tagname, ...)
-    return make_tag(print, tagname, split(flatten({
-      ...
-    })))
-  end
-  return environment
+end)
+local xhtml = xml:derive()
+xhtml.environment.escape = function(str)
+  return str:upper()
 end
-local make
-if is51 then
-  make = function(environment)
-    return function(fnc)
-      assert(type(fnc) == 'function', 'wrong argument to render, expecting function')
-      setfenv(fnc, environment)
-      return function(out, ...)
-        if out == nil then
-          out = print
-        end
-        environment.print = out
-        return fnc(...)
-      end
-    end
-  end
-else
-  make = function(environment)
-    return function(fnc)
-      assert(type(fnc) == 'function', 'wrong argument to render, expecting function')
-      do
-        local upvaluejoin = debug.upvaluejoin
-        local _ENV = environment
-        upvaluejoin(fnc, 1, (function()
-          return aaaa()
-        end), 1)
-      end
-      return function(out, ...)
-        if out == nil then
-          out = print
-        end
-        environment.print = out
-        return fnc(...)
-      end
-    end
-  end
-end
-local render
-render = function(out, fnc)
-  return build(fnc)(out)
-end
-local attrib
-attrib = function(args)
-  local res = setmetatable({ }, {
-    __tostring = function(self)
-      local tab
-      do
-        local _accum_0 = { }
-        local _len_0 = 1
-        for key, value in pairs(self) do
-          if type(value) == 'string' or type(value) == 'number' then
-            _accum_0[_len_0] = tostring(key) .. "=\"" .. tostring(value) .. "\""
-            _len_0 = _len_0 + 1
-          end
-        end
-        tab = _accum_0
-      end
-      return #tab > 0 and ' ' .. table.concat(tab, ' ') or ''
-    end
-  })
-  for key, value in pairs(args) do
-    if type(key) == 'string' then
-      res[key] = value
-      local r = true
-    end
-  end
-  return res
-end
-local handle
-handle = function(print, args)
-  for _index_0 = 1, #args do
-    local arg = args[_index_0]
-    local _exp_0 = type(arg)
-    if 'table' == _exp_0 then
-      handle(arg)
-    elseif 'function' == _exp_0 then
-      arg()
-    else
-      print(tostring(arg))
-    end
-  end
-end
-local environment = {
-  xml = env(function(print, tagname, inner, args)
-    print("<" .. tostring(tagname) .. tostring(attrib(args)) .. tostring(#inner == 0 and ' /' or '') .. ">")
-    if not (#inner == 0) then
-      handle(print, inner)
-    end
-    if not ((#inner == 0)) then
-      return print("</" .. tostring(tagname) .. ">")
-    end
-  end),
-  html = env(function(print, tagname, inner, args)
-    if not (void[tagname] and #inner == 0) then
-      print("<" .. tostring(tagname) .. tostring(attrib(args)) .. ">")
-      if not (#inner == 0) then
-        handle(print, inner)
-      end
-      return print("</" .. tostring(tagname) .. ">")
-    else
-      return print("<" .. tostring(tagname) .. tostring(attrib(args)) .. ">")
-    end
-  end)
-}
-environment.html.html5 = function()
-  return environment.html.print('<!doctype html5>')
-end
-return {
-  environment = environment,
-  xml = make(environment.xml),
-  html = make(environment.html)
-}
+print("XML Environment:   " .. tostring(xml.environment))
+print("XHTML Environment: " .. tostring(xhtml.environment))
+return xhtml.environment.html("test")
--- xml.moon
+++ xml.moon
@@ -1,5 +1,77 @@
 is51 = _VERSION == 'Lua 5.1'
 
+global = _ENV or _G
+make_environment = (node_handler) ->
+	environment = do
+		setmetatable {}, {
+			__index: (key) =>
+				switch key
+					when 'escape'
+						(...) -> ...
+					else
+						global[key] or (...) ->
+							@.node(key, ...)
+		}
+	
+	if is51
+		setfenv(1, environment)
+	_ENV = _ENV and environment
+
+	flatten = (tab, flat={}) ->
+		for key, value in pairs tab
+			if type(key)=="number"
+				if type(value)=="table"
+					flatten(value, flat)
+				else
+					flat[#flat+1]=value
+			else
+				if type(value)=="table"
+					flat[key] = table.concat value ' '
+				else
+					flat[key] = value
+		flat
+	
+	inner = (content) ->
+		print "Environment: "..tostring _ENV
+		for entry in *content
+			switch type entry
+				when 'string' print escape entry
+				when 'function' entry!
+				else print escape tostring entry
+
+	export node = (tagname, ...) ->
+		arguments = flatten{...}
+		content = {}
+		-- Remove numeric indices into *content*
+		for k,v in ipairs arguments do
+			content[k]=v
+			arguments[k]=nil
+		local handle_content
+		if #content > 0
+			handle_content = -> inner content
+		else
+			handle_content = nil
+		node_handler(print, tagname, arguments, handle_content)
+	environment
+
+call = (fnc) =>
+	if type(fnc) != 'function'
+		error "Argument must be a function!", 3
+	error "This land is peaceful, it's inhabitants kind. But thou dost not belong.", 3
+
+derive = =>
+	derivate = language(@node_handler)
+	meta = getmetatable derivate -- Pitfall: Can't just set __index as it already exists
+	meta.__index = (key) -> @[key] or meta.__index
+	return derivate
+
+language = (node_handler) ->
+	setmetatable {
+		:node_handler
+		:derive
+		environment: make_environment node_handler
+		}, {__call: call}
+
 void = {key,true for key in *{
 	"area", "base", "br", "col"
 	"command", "embed", "hr", "img"
@@ -15,111 +87,30 @@ escapes = {
 	["'"]: '&#039;'
 }
 
-global = _ENV or _G
-env = (make_tag) ->
-	environment = do
-		setmetatable {}, {
-			__index: (key) =>
-				global[key] or (...) ->
-					@.tag(key, ...)
-		}
+--	export escape = (str) ->
+--		(=>@) tostring(str)\gsub [[[<>&]'"]], escapes
 
-	if is51
-		setfenv(1, environment)
-	_ENV = _ENV and environment
+xml = language (print, tag, args, inner) ->
+	args = table.concat ['#{key}="#{value}"' for key, value in pairs args], ' '
+	if inner
+		print "<#{tag} #{args}>"
+		inner!
+		print "</#{tag}>"
+	else
+		print "<#{tag} #{args}/>"
 
-	export escape = (value) ->
-		(=>@) tostring(value)\gsub [[[<>&]'"]], escapes
-
-	split = (tab) ->
-		ary = {}
-		for k,v in ipairs(tab) do
-			ary[k]=v
-			tab[k]=nil
-		return ary, tab
-
-	flatten = (tab, flat={}) ->
-		for key, value in pairs tab
-			if type(key)=="number"
-				if type(value)=="table"
-					flatten(value, flat)
-				else
-					flat[#flat+1]=value
-			else
-				if type(value)=="table"
-					flat[key] = table.concat value ' '
-				else
-					flat[key] = value
-		flat
-
-	export tag = (tagname, ...) ->
-		make_tag(print, tagname, split flatten {...})
-
-	return environment
-
-make = if is51 then
-  (environment) ->
-    (fnc) ->
-      assert(type(fnc)=='function', 'wrong argument to render, expecting function')
-      setfenv(fnc, environment)
-      return (out=print, ...) ->
-        environment.print = out
-        return fnc(...)
-else
-  (environment) ->
-    (fnc) ->
-      assert(type(fnc)=='function', 'wrong argument to render, expecting function')
-      do
-        upvaluejoin = debug.upvaluejoin
-        _ENV = environment
-        upvaluejoin(fnc, 1, (-> aaaa!), 1) -- Set environment
-      return (out=print, ...) ->
-        environment.print = out
-        return fnc(...)
-
-render = (out, fnc) ->
-	build(fnc)(out)
-
-attrib = (args) ->
-	res = setmetatable {}, __tostring: =>
-		tab = ["#{key}=\"#{value}\"" for key, value in pairs(@) when type(value)=='string' or type(value)=='number']
-		#tab > 0 and ' '..table.concat(tab,' ') or ''
-	for key, value in pairs(args)
-		if type(key)=='string'
-			res[key] = value
-			r = true
-	return res
-
---- Handles the inner HTML of a tag
-handle = (print, args) ->
-	for arg in *args
-		switch type(arg)
-			when 'table'
-				handle arg
-			when 'function'
-				arg!
-			else
-				print tostring arg
-
-environment = {
-	xml: env (print, tagname, inner, args) ->
-		print "<#{tagname}#{attrib args}#{#inner==0 and ' /' or ''}>"
-		handle print, inner unless #inner==0
-		print "</#{tagname}>" unless (#inner==0)
-	html: env (print, tagname, inner, args) ->
-		unless void[tagname] and #inner==0
-			print "<#{tagname}#{attrib args}>"
-			handle print, inner unless #inner==0
-			print "</#{tagname}>"
-		else
-			print "<#{tagname}#{attrib args}>"
-}
-environment.html.html5 = ->
-	environment.html.print '<!doctype html5>'
-
-
-{
-	:environment
-	xml: make environment.xml
-	html: make environment.html
-}
+html = language (print, tag, args, inner) ->
+	args = ['#{key}="#{value}"' for key, value in pairs args]
+	if inner
+		print "<#{tag} #{args}>"
+		inner!
+		print "</#{tag}>"
+	else
+		print "<#{tag} #{args} />"
+	
+xhtml = xml\derive!
+-- xhtml = language xml.node_handler
+xhtml.environment.escape = (str) -> str\upper!
+print "XML Environment:   "..tostring xml.environment
+print "XHTML Environment: "..tostring xhtml.environment
+xhtml.environment.html "test"
